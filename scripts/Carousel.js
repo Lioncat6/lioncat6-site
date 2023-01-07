bf513019dd560e5271b6a2e2ca54e144db96eddb
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2023-01-06 22:07:16 -0600

    Update Carousel.js

--- scripts/Carousel.js
+++ scripts/Carousel.js
@@ -29,6 +29,7 @@ function currentSlideThree() {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
+  console.log(slides)
   let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
@@ -38,7 +39,7 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style = "display: block";  
+  slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
 setInterval(() => {plusSlides(1)}, 5000);
