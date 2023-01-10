c54f1c87275bee3620ede04e10e411060733f7cb
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2023-01-10 16:16:54 -0600

    aaa

--- scripts/Menu.js
+++
@@ -1,8 +0,0 @@
-function menuTrigger() {
-    menuBtn.getElementById('menuBtn').classList.toggle('change'); 
-    if (menuBtn.getElementById('menuBtn').classList.contains('change')) {
-    document.getElementById('dmenu').style.display = 'block'
-    } else {
-    document.getElementById('dmenu').style.display = 'none'
-    };
-}
