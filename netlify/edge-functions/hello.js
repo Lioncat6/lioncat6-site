aa0422a00436ca574bd1009337de5c34167c1ebd
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2023-01-19 14:35:18 -0600

    test

---
+++ netlify/edge-functions/hello.js
@@ -0,0 +1,9 @@
+export default async(request, context) => {
+    return new Response("This was generated with edge with JS!",
+    {
+        headers: {
+            "context-type": text/html
+        }
+    }
+    )
+}
