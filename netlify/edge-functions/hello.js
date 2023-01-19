88a46e89f3e5a12b1b2e27fe46c3f2640d9f3b02
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2023-01-19 14:39:33 -0600

    fixed lol

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -2,7 +2,7 @@ export default async(request, context) => {
     return new Response("This was generated with edge with JS!",
     {
         headers: {
-            "context-type": text/html
+            "context-type": "text/html"
         }
     }
     )
