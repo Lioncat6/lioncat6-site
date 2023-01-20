0e0a44dc26b65a84140628576c3623b01daacea0
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2023-01-19 18:05:31 -0600

    testing

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,7 +1,35 @@
 export default async(request, context) => {
-    var date = new Date();
-    var current_hour = date.getSeconds();
-    return new Response("This was generated with edge with JS! The current hour is:"+current_hour,
+    var getJSON = function(url, callback) {
+
+        var xmlhttprequest = new XMLHttpRequest();
+        xmlhttprequest.open('GET', url, true);
+        xmlhttprequest.responseType = 'json';
+    
+        xmlhttprequest.onload = function() {
+    
+            var status = xmlhttprequest.status;
+    
+            if (status == 200) {
+                callback(null, xmlhttprequest.response);
+            } else {
+                callback(status, xmlhttprequest.response);
+            }
+        };
+    
+        xmlhttprequest.send();
+    };
+    
+    getJSON('https://www.planetminecraft.com/texture-pack/behavior-pack-glow-blocks-for-radiant-pack-rtx/stats',  function(err, data) {
+    
+        if (err != null) {
+            console.error(err);
+        } else {
+    
+           var display = data;
+        }
+      console.log(); 
+    });
+    return new Response("This was generated with edge with JS!"+display,
     {
         headers: {
             "context-type": "text/html"
