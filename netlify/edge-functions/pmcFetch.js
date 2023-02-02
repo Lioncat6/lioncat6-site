5dd709a123a86bdc6d2dc1b3d38061d6616221d7
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2023-02-02 14:24:36 -0600

    a

--- netlify/edge-functions/pmcFetch.js
+++ netlify/edge-functions/pmcFetch.js
@@ -1,5 +1,3 @@
-import { Context } from "https://edge.netlify.com";
-
 export default async (request, context) => {
 
   const pmcStats = await fetch("https://planetminecraft.com/exture-pack/behavior-pack-glow-blocks-for-radiant-pack-rtx/stats", {
