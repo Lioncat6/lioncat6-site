57eb7ddbe33aed69aeca88cc98c1a421f6e689a6
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2023-02-02 14:36:21 -0600

    problem solved.

--- netlify/edge-functions/pmcFetch.js
+++ netlify/edge-functions/pmcFetch.js
@@ -1,10 +1,6 @@
 export default async (request, context) => {
 
-  const pmcStats = await fetch("https://planetminecraft.com/exture-pack/behavior-pack-glow-blocks-for-radiant-pack-rtx/stats", {
-    "headers": {
-      "Accept": "application/json"
-    }
-  });
+  const pmcStats = await fetch("https://planetminecraft.com/exture-pack/behavior-pack-glow-blocks-for-radiant-pack-rtx/stats");
   
   return new Response(pmcStats.text(), 
     {
