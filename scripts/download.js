d687a376e986648281a0435b2312f2729cb007a0
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2024-09-14 01:41:37 -0500

    maybe

--- scripts/download.js
+++ scripts/download.js
@@ -3,14 +3,8 @@ function showAd(url){
       const box = document.getElementById('adBox');
       
       // Inject HTML content into the box
-      box.innerHTML = `
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8744471424191306"
-       crossorigin="anonymous"></script>
-    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-8744471424191306" data-ad-slot="7534387079" data-ad-format="auto" data-full-width-responsive="true"></ins>
-                  <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script>
-  
-          <button id="hideOverlay">Close</button>`;
       
+      (adsbygoogle = window.adsbygoogle || []).push({});
       // Add event listener to the new close button
       document.getElementById('hideOverlay').addEventListener('click', function() {
           overlay.classList.remove('show');
